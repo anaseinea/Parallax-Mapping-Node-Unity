47e66a744067f9a46689999953882d4a977fb931
anaseinea <anasainea@gmail.com>
2019-01-31 17:37:07 -0600

    adding readme with the node

---
+++ README.md
@@ -0,0 +1,9 @@
+# Parallax-Mapping-in-Unity-Shader-Graph
+
+Node code used in the tutorial
+
+[Tutorial](https://youtu.be/)
+
+[My Material Pack](https://www.assetstore.unity3d.com/#!/content/136692)
+
+![Screenshot](http://anaseinea.github.io/Parallax-Mapping-Node-Unity/Preview_image.png)
---
+++ paralaxNode.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEditor.ShaderGraph;
+using System.Reflection;
+[Title("Custom", "paralax")]
+	public class ParalaxNode : CodeFunctionNode
+{
+	public ParalaxNode()
+	{
+		name = "paralax";
+	}
+
+	protected override MethodInfo GetFunctionToConvert()
+	{
+		return GetType().GetMethod("MyCustomFunction",
+			BindingFlags.Static | BindingFlags.NonPublic);
+	}
+	static string MyCustomFunction(
+		[Slot(0, Binding.None)] Vector1 heightScale,
+		[Slot(1, Binding.TangentSpaceViewDirection)] Vector3 viewDir,
+    [Slot(2, Binding.None)] Texture2D HeightTex,
+		[Slot(3, Binding.MeshUV0)] Vector2 uv,
+    [Slot(4, Binding.None)] SamplerState sampleState,
+		[Slot(5, Binding.None)] out Vector2 Out)
+
+	{
+		Out = uv; 
+		return
+			@"
+{
+  // determine required number of layers
+  const float minLayers = 30;
+  const float maxLayers = 60;
+  float numLayers = lerp(maxLayers, minLayers, abs(dot(float3(0, 0, 1), viewDir)));
+
+  float numSteps = numLayers;//60.0f; // How many steps the UV ray tracing should take
+  float height = 1.0;
+  float step = 1.0 / numSteps;
+  
+  float2 offset = uv.xy;
+  float4 HeightMap = HeightTex.Sample(sampleState, offset);
+  
+  float2 delta = viewDir.xy * heightScale / (viewDir.z * numSteps);
+  
+  // find UV offset
+  for (float i = 0.0f; i < numSteps; i++) {
+    if (HeightMap.r < height) {
+      height -= step;
+      offset += delta;
+      HeightMap = HeightTex.Sample(sampleState, offset);
+    } else {
+      break;
+    }
+  }
+  Out = offset;
+}  
+";
+	}
+
+
+}
